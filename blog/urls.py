9d08e32dfe8acb7505fd138c5b3117248770bcf5
Annabelle <Annabelle>
2017-07-31 15:57:57 -0400

    blog added

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.portfolio, name='portfolio'),
+    url(r'^posts/', views.post_list, name='post_list'),
 ]
